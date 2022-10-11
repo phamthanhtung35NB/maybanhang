6640486612b10c28fb38530a2cc4633e40bf308b
Phạm Thanh Tùng <81307877+tungpham010203@users.noreply.github.com>
2022-10-11 23:46:08 +0700

    Update maybanhang.py

--- maybanhang.py
+++ maybanhang.py
@@ -4,25 +4,26 @@ lcd = LCD()
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
-
-
-def safe_exit(signum, frame):
-    exit(1)
-try:
-    signal(SIGTERM, safe_exit)
-    signal(SIGHUP, safe_exit)
-    lcd.text("Xin Chao!", 1)
-    lcd.text("Moi Ban Quet The", 2)
-    pause()
-except KeyboardInterrupt:
-    pass
-finally:
-    lcd.clear()
-
-try:
+while True:
+    #quet the
+    try:
         id, text = reader.read()
         print(id)
         print(text)
-finally:
-        GPIO.cleanup()    
-
+    finally:
+        GPIO.cleanup() 
+            
+    #loi moi quet the
+    
+    def safe_exit(signum, frame):
+        exit(1)
+    try:
+        signal(SIGTERM, safe_exit)
+        signal(SIGHUP, safe_exit)
+        lcd.text("Xin Chao!", 1)
+        lcd.text("Moi Ban Quet The", 2)
+        pause()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        lcd.clear()
